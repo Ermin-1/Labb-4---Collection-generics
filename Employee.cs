c92d7ec01e4620008c03f4b14c966e46a54b28f6
Ermin Husic <Ermin.Husic@live.se>
2023-11-01 09:07:23 +0100

    Add project files.

---
+++ Employee.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labb_4___Collection_generics
+{
+    internal class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Gender { get; set; }
+        public int Salary { get; set; }
+
+        public Employee(int Id, string Name, string Gender, int Salary)
+        {
+            this.Id = Id;
+            this.Name = Name;
+            this.Gender = Gender;
+            this.Salary = Salary;
+    
+        }
+
+        public override string ToString()
+        {
+            return $"{Id} - {Name} - {Gender} - {Salary}";
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Labb_4___Collection_generics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee em1 = new Employee(111, "Ermin", "Male", 35000);
+            Employee em2 = new Employee(222, "Sharam", "Male", 34000);
+            Employee em3 = new Employee(333, "Markus", "Male", 33000);
+            Employee em4 = new Employee(444, "Christian", "Male", 32000);
+            Employee em5 = new Employee(555, "Fredrik", "Male", 31000);
+
+            Stack<Employee> Emstack = new Stack<Employee>();
+            Emstack.Push(em1);
+            Emstack.Push(em2);
+            Emstack.Push(em3);
+            Emstack.Push(em4);
+            Emstack.Push(em5);
+
+
+
+        foreach(Employee item in Emstack) 
+            {
+                Console.WriteLine("Items left in Stack = " + Emstack.Count);
+                Console.WriteLine(item + " ");
+            
+            }
+
+        }
+    }
+}
